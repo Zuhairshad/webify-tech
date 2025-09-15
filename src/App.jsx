ee8bfcdc0d07669fecf193b3451145b34d8fd4b3
Zuhair Shad <123764183+Zuhairshad@users.noreply.github.com>
2025-09-15 15:47:33 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Work from "./pages/Work.jsx"
 import CaseStudy from "./pages/CaseStudy.jsx";
 import Contact from "./pages/Contact.jsx"
 import RequestProposal from "./pages/RequestProposal.jsx";
-import WebDesign from "./pages/WebDesign.jsx";
+import websiteDesign from "./pages/websiteDesign.jsx";
 import Navbar from "./components/Navbar.jsx";
 import UIUX from "./pages/UIUX.jsx";
 import DigitalMarketing from "./pages/DigitalMarketing.jsx";
@@ -25,7 +25,7 @@ export default function App() {
         <Route path="/work/:id" element={<CaseStudy />} />
         <Route path="/contact" element={<Contact/>} />
         <Route path="/proposalrequest" element={<RequestProposal/>} />
-        <Route path="/services/web-development" element={<WebDesign />} />
+        <Route path="/services/web-development" element={<websiteDesign />} />
         <Route path="/services/SEO" element={<SEO/>}/>
         <Route path="/services/UIUX" element={<UIUX />} />
         <Route path="/services/digital-marketing" element={<DigitalMarketing />} />
